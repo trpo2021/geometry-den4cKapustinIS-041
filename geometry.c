8c357994aaf1e0e6c08d94244a9328270c4618dd
Денис Капустин <denis-ka-2016@mail.ru>
2021-04-02 00:54:39 +0300

    Added a function for calculating and displaying the area and perimeter

--- geometry.c
+++ geometry.c
@@ -22,7 +22,24 @@ void cirlce_output(char* str)
 {
     fputs(str, stdout);
 }
-
+void perimeter_n_area(char* str, int max_symb)
+{
+    int r = 0;
+    int i, k;
+    char radius[10];
+    float area, perimeter;
+    for (i = 0; i < max_symb; i++) {
+        if ((str[i] == ',') && (str[i + 1] == ' ')) {
+            for (k = i; str[k + 1] != ')'; k++) {
+                radius[k - i] = str[k + 1];
+            }
+        };
+    }
+    r = atoi(radius);
+    area = M_PI * r * r;
+    perimeter = 2 * M_PI * r;
+    printf("area = %.3f\nperimeter = %.3f\n", area, perimeter);
+}
 int main()
 {
     int max_symb = 40;
@@ -32,5 +49,6 @@ int main()
     fgets(sourse_str, max_symb, mf);
     correct_spelling(sourse_str, max_symb);
     cirlce_output(sourse_str);
+    perimeter_n_area(sourse_str, max_symb);
     return 0;
 }
